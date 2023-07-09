6280a8b1017b02a1cc2066334a42fc29546fe551
NastaBystritskaya <nasta.bystritskaya@gmail.com>
2023-07-09 19:40:18 +0300

    first commit

---
+++ задача 48/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задайте двумерный массив n на m. Каждый элемент находится по формуле А(mn) = m + n. Выведите полученный массив на экран
+
+void PrintArray() {
+    int[,] Array = new int[3, 4];
+    for (int j = 0; j < 3; j++)
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            Array[j, i] = i + j;
+            System.Console.Write(Array[j, i]);
+        }
+        System.Console.WriteLine();
+    }
+}
+PrintArray();
